7af5aff25d97b3eff1ff25f0624a9dbadb3e9da1
pratik0217 <jadhavpratik0208@gmail.com>
2025-08-07 20:10:54 +0530

    Completed home page without animation etc.

---
+++ src/component/SoicalMediaBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default function SoicalMediaBar() {
+  return (
+    <div className='social-media'>
+      <div className="socialMedia-link">
+        <div className="text-media">
+          <p>Follow Me</p>
+        </div>
+        <div className="socialMedia-icon">
+          <ul>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
